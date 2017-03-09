c9b086ebfdbedf3d54586d7134703dc973e27b5f
CSKim <deusta@naver.com>
2017-03-09 15:54:14 +0900

    New activity added. 170309.

---
+++ app/src/main/java/com/example/iveci/macc/Main3Activity.java
@@ -0,0 +1,45 @@
+package com.example.iveci.macc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Main3Activity extends AppCompatActivity {
+    EditText t1,t2;
+    Button b1,b2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        init();
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String year = t1.getText().toString();
+                int caly = 2017 - Integer.parseInt(year) + 1;
+                Toast.makeText(getApplicationContext(),
+                        "당신의 나이는 "+ caly + " 입니다.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String age = t2.getText().toString();
+                int cala = 2017 - Integer.parseInt(age) + 1;
+                Toast.makeText(getApplicationContext(),
+                        "당신이 태어난 해는 " + cala + "년 입니다.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    void init(){
+        t1 = (EditText) findViewById(R.id.tyear);
+        t2 = (EditText) findViewById(R.id.tnum);
+        b1 = (Button) findViewById(R.id.button);
+        b2 = (Button) findViewById(R.id.button2);
+    }
+}
--- app/src/main/java/com/example/iveci/macc/MainActivity.java
+++ app/src/main/java/com/example/iveci/macc/MainActivity.java
@@ -7,12 +7,12 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button b1;
+    Button b1,b2,b3,b4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b1 = (Button)findViewById(R.id.b1);
+        init();
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -21,6 +21,29 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(MainActivity.this, Main3Activity.class);
+                startActivity(a);
+            }
 
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(MainActivity.this, Main4Activity.class);
+                startActivity(a);
+            }
+        });
     }
+        void init(){
+
+            b1 = (Button)findViewById(R.id.b1);
+            b2 = (Button)findViewById(R.id.button4);
+            b3 = (Button)findViewById(R.id.button5);
+            b4 = (Button)findViewById(R.id.button6);
+        }
+
+
 }
